79e6c340938907df80539328088542fa8eefd884
beardofdoom <marcin.jusiak@gmail.com>
2019-01-28 12:40:54 +0100

    Updates

---
+++ precache-manifest.bcdd1a60144aa24782d8183754a86b00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "09bc7c589abf03be5c11bafaee0692ff",
+    "url": "/react-weather/static/media/bg.09bc7c58.jpg"
+  },
+  {
+    "revision": "8ad3cef1c2506df250b9",
+    "url": "/react-weather/static/js/runtime~main.8ad3cef1.js"
+  },
+  {
+    "revision": "48285cb64817475589aa",
+    "url": "/react-weather/static/js/main.48285cb6.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/react-weather/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "48285cb64817475589aa",
+    "url": "/react-weather/static/css/main.a83a8444.chunk.css"
+  },
+  {
+    "revision": "c5116376029d760f2c7724be9233a98b",
+    "url": "/react-weather/index.html"
+  }
+];
